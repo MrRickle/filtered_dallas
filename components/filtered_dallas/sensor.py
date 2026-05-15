8a22ec4e53647d772cf1a73c029739ee9c86aef0
Rick Hallock <rick@hallocks.us>
2026-05-15 15:36:34 -0500

    more

--- components/filtered_dallas/sensor.py
+++ components/filtered_dallas/sensor.py
@@ -30,7 +30,7 @@ CONFIG_SCHEMA = sensor.sensor_schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
 
-    await cg.register_sensor(var, config)
+    await sensor.register_sensor(var, config)
 
     cg.add(var.set_spike_threshold(config[CONF_SPIKE_THRESHOLD]))
     cg.add(var.set_max_rejects(config[CONF_MAX_REJECTS]))
