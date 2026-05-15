3738a4d90e57c17eab79b767b4ffdc9e7e3f3ba2
Rick Hallock <rick@hallocks.us>
2026-05-15 15:32:08 -0500

    more

--- components/filtered_dallas/sensor.py
+++ components/filtered_dallas/sensor.py
@@ -6,15 +6,23 @@ from esphome.const import CONF_ID
 DEPENDENCIES = ["sensor"]
 
 filtered_dallas_ns = cg.esphome_ns.namespace("filtered_dallas")
-FilteredDallasSensor = filtered_dallas_ns.class_("FilteredDallasSensor", sensor.Sensor)
+
+FilteredDallasSensor = filtered_dallas_ns.class_(
+    "FilteredDallasSensor",
+    sensor.Sensor,
+)
 
 CONF_SPIKE_THRESHOLD = "spike_threshold"
 CONF_MAX_REJECTS = "max_rejects"
 
-CONFIG_SCHEMA = sensor.sensor_schema(FilteredDallasSensor).extend(
+CONFIG_SCHEMA = sensor.sensor_schema(
+    FilteredDallasSensor
+).extend(
     {
         cv.GenerateID(): cv.declare_id(FilteredDallasSensor),
+
         cv.Optional(CONF_SPIKE_THRESHOLD, default=15.0): cv.float_,
+
         cv.Optional(CONF_MAX_REJECTS, default=3): cv.int_,
     }
 )
@@ -22,7 +30,6 @@ CONFIG_SCHEMA = sensor.sensor_schema(FilteredDallasSensor).extend(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
 
-    await cg.register_component(var, config)
     await cg.register_sensor(var, config)
 
     cg.add(var.set_spike_threshold(config[CONF_SPIKE_THRESHOLD]))
