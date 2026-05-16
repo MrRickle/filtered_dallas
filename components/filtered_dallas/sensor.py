b025353ec2200ab315965a80cddfa439ddc560f7
Rick Hallock <rick@hallocks.us>
2026-05-16 07:04:22 -0500

    4 C

--- components/filtered_dallas/sensor.py
+++ components/filtered_dallas/sensor.py
@@ -25,7 +25,7 @@ CONFIG_SCHEMA = sensor.sensor_schema(
     {
         cv.GenerateID(): cv.declare_id(FilteredDallasSensor),
 
-        cv.Optional(CONF_SPIKE_THRESHOLD, default=2.0): cv.float_,
+        cv.Optional(CONF_SPIKE_THRESHOLD, default=4.0): cv.float_,
 
         cv.Optional(CONF_MAX_REJECTS, default=3): cv.int_,
     }
